b334136c52cdf41983d66b59b1fd7f43bb14acb8
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-11-03 13:13:18 +1300

    Add under construction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 // @flow
 
-import React from "react";
+import React from 'react';
+import gif from '../../static/under_construction_graphic.gif';
 
-const Index = () => <div>Hello world!</div>;
+const Index = () => (
+    <div>
+        <img alt="under construction" src={gif} />
+    </div>
+);
 
 export default Index;
